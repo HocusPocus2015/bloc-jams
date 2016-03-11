5c689c9af39e5df8c433a58a8a17097e89e8cda0
Alice Suckling <alicesuckling@live.co.uk>
2016-03-11 12:50:54 +0000

    DOM scripting collection view

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,16 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-  var revealPoint = function(index) {
+var animatePoints = function (points) {
+  var revealPoint = function (index) {
     points[index].style.opacity = 1;
     points[index].style.transform = "scaleX(1) translateY(0)";
     points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";   
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
   };
-    
-  for (var i = 0; i < points.length; i++) {
+  
+  for (var i=0;i< points.length;i++){
     revealPoint(i);
-  }
+  };
 };
 
 window.onload = function() {
@@ -20,10 +20,10 @@ window.onload = function() {
     
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    
+//  var scrollDistance = this.getBoundingClientRect().top - window.innerHeight + 200;
   window.addEventListener("scroll", function(event) {
-    if (document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);   
+    if (document.documentElement.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
     }
   });
 }
